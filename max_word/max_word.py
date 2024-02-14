779111fb9ebde5bebb888c3315dd7abd67dabc65
DyingFirst <v.artyo@mail.ru>
2024-02-14 13:54:18 +0300

    hueta

--- max_word/max_word.py
+++ max_word/max_word.py
@@ -1,10 +1,22 @@
-def len_of_word ():
-    content = ""
-    max = ""
-    with open('example.txt', 'r') as file:
-        content = file.read
-    for i in content:
-        if i > max:
-            max = i
-    return len(max)
-print(len_of_word())
+import re
+def len_of_word():  
+    longest_words = []
+    max_length = 0
+
+    # Открываем файл и читаем его содержимое
+    with open("./example.txt", 'r') as file:
+        text = file.read()
+    
+    # Используем регулярное выражение для извлечения слов из текста
+    words = re.findall(r'\b\w+\b', text)
+
+    # Находим максимальную длину слова
+    for word in words:
+        word_length = len(word)
+        if word_length > max_length:
+            max_length = word_length
+            longest_words = [word]
+        elif word_length == max_length:
+            longest_words.append(word)
+
+    print(longest_words)
